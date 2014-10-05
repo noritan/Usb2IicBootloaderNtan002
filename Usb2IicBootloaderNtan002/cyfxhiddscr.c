015741a6453ad8a3965051eef131325c1d701f45
noritan <github@noritan.org>
2014-10-05 20:18:21 +0900

    Prepared descriptors for SS Revised SS descriptors recognized as SS HID device. SS is not still been enabled.

--- Usb2IicBootloaderNtan002/cyfxhiddscr.c
+++ Usb2IicBootloaderNtan002/cyfxhiddscr.c
@@ -91,7 +91,7 @@ const uint8_t CyFxUSBBOSDscr[] __attribute__ ((aligned (32))) =
     CY_U3P_DEVICE_CAPB_DESCR,       /* Device capability type descriptor */
     CY_U3P_SS_USB_CAPB_TYPE,        /* SuperSpeed device capability type */
     0x00,                           /* Supported device level features  */
-    0x0E,0x00,                      /* Speeds supported by the device : SS, HS and FS */
+    0x06,0x00,                      /* Speeds supported by the device : HS and FS */
     0x03,                           /* Functionality support */
     0x0A,                           /* U1 Device Exit latency */
     0xFF,0x07                       /* U2 Device Exit latency */
@@ -117,53 +117,62 @@ const uint8_t CyFxUSBSSConfigDscr[] __attribute__ ((aligned (32))) =
     /* Configuration descriptor */
     0x09,                           /* Descriptor size */
     CY_U3P_USB_CONFIG_DESCR,        /* Configuration descriptor type */
-    0x2C,0x00,                      /* Length of this descriptor and all sub descriptors */
+    0x35,0x00,                      /* Length of this descriptor and all sub descriptors */
     0x01,                           /* Number of interfaces */
     0x01,                           /* Configuration number */
-    0x00,                           /* COnfiguration string index */
+    0x04,                           /* Configuration string index */
     0x80,                           /* Config characteristics - Bus powered */
-    0x32,                           /* Max power consumption of device (in 8mA unit) : 400mA */
+    0x0C,                           /* Max power consumption of device (in 8mA unit) : 96mA */
 
     /* Interface descriptor */
     0x09,                           /* Descriptor size */
     CY_U3P_USB_INTRFC_DESCR,        /* Interface Descriptor type */
     0x00,                           /* Interface number */
     0x00,                           /* Alternate setting number */
-    0x02,                           /* Number of end points */
-    0xFF,                           /* Interface class */
+    0x02,                           /* Number of endpoints */
+    0x03,                           /* Interface class HID */
     0x00,                           /* Interface sub class */
     0x00,                           /* Interface protocol code */
-    0x00,                           /* Interface descriptor string index */
+    0x05,                           /* Interface descriptor string index */
+
+    /* HID Class Descriptor */
+    0x09,                           /* HID Class Descriptor Length */
+    CY_U3P_USB_HID_DESCR,           /* DescriptorType: HID_CLASS */
+    0x11, 0x01,                     /* bcdHID */
+    0x21,                           /* bCountryCode */
+    0x01,                           /* bNumDescriptors */
+    CY_U3P_USB_REPORT_DESCR,        /* bDescriptorType */
+    0x2B, 0x00,                     /* wDescriptorLength */
 
     /* Endpoint descriptor for producer EP */
     0x07,                           /* Descriptor size */
     CY_U3P_USB_ENDPNT_DESCR,        /* Endpoint descriptor type */
     CY_FX_EP_PRODUCER,              /* Endpoint address and description */
-    CY_U3P_USB_EP_BULK,             /* Bulk endpoint type */
-    0x00,0x04,                      /* Max packet size = 1024 bytes */
-    0x00,                           /* Servicing interval for data transfers : 0 for bulk */
+    CY_U3P_USB_EP_INTR,             /* Interrupt endpoint type */
+    CY_FX_EP_PACKET_SIZE,0x00,      /* Max packet size = 64 bytes */
+    0x01,                           /* Servicing interval for data transfers : 1ms */
 
     /* Super speed endpoint companion descriptor for producer EP */
     0x06,                           /* Descriptor size */
     CY_U3P_SS_EP_COMPN_DESCR,       /* SS endpoint companion descriptor type */
     (CY_FX_EP_BURST_LENGTH - 1),    /* Max no. of packets in a burst(0-15) - 0: burst 1 packet at a time */
     0x00,                           /* Max streams for bulk EP = 0 (No streams) */
-    0x00,0x00,                      /* Service interval for the EP : 0 for bulk */
+    0x40,0x00,                      /* Service interval for the EP : 0 for bulk */
 
     /* Endpoint descriptor for consumer EP */
     0x07,                           /* Descriptor size */
     CY_U3P_USB_ENDPNT_DESCR,        /* Endpoint descriptor type */
     CY_FX_EP_CONSUMER,              /* Endpoint address and description */
-    CY_U3P_USB_EP_BULK,             /* Bulk endpoint type */
-    0x00,0x04,                      /* Max packet size = 1024 bytes */
-    0x00,                           /* Servicing interval for data transfers : 0 for Bulk */
+    CY_U3P_USB_EP_INTR,             /* Interrupt endpoint type */
+    CY_FX_EP_PACKET_SIZE,0x00,      /* Max packet size = 64 bytes */
+    0x01,                           /* Servicing interval for data transfers : 1ms */
 
     /* Super speed endpoint companion descriptor for consumer EP */
     0x06,                           /* Descriptor size */
     CY_U3P_SS_EP_COMPN_DESCR,       /* SS endpoint companion descriptor type */
     (CY_FX_EP_BURST_LENGTH - 1),    /* Max no. of packets in a burst(0-15) - 0: burst 1 packet at a time */
     0x00,                           /* Max streams for bulk EP = 0 (No streams) */
-    0x00,0x00                       /* Service interval for the EP : 0 for bulk */
+    0x40,0x00                       /* Service interval for the EP : 0 for bulk */
 };
 
 /* Standard high speed configuration descriptor */
